aae4029264e470d3e3bf89042fc0821ad4b82252
cristianFedora <cristianoso19@gmail.com>
2023-09-29 23:50:51 -0500

    Agregar scripts a nuestro tema, de internet y propios en function.php Creamos custom.js para archivo de javascript Header.php agregamos un navbar Agregamos un logo.png para nuestro navbar dentro de la carpeta creada assets/img

--- wordpress-data/wp-content/themes/prof/functions.php
+++ wordpress-data/wp-content/themes/prof/functions.php
@@ -11,6 +11,12 @@
         wp_register_style('onest','https://fonts.googleapis.com/css2?family=Onest:wght@200;400;700&display=swap','','1.0','all');
 
         wp_enqueue_style('estilos', get_stylesheet_uri(), array('daysiUI','onest'),'1.0', 'all');
+        // Creamos un script cdn (internet) 
+        wp_register_script('daysiUIjs','https://cdn.tailwindcss.com','','3.8.1',true); //true para que se ejecute en el footer
+        wp_enqueue_script('daysiUIjs');
+        // Creamos un script propio 
+        // get_template_directory_uri() devuelve el path del tema
+        wp_enqueue_script('custom',get_template_directory_uri().'/assets/js/custom.js','','1.0',true);
 
     }
     add_action('wp_enqueue_scripts','assets');
--- wordpress-data/wp-content/themes/prof/header.php
+++ wordpress-data/wp-content/themes/prof/header.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="light">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prof gifts</title>
-    <?php wp_head()?>
+    <?php wp_head() ?>
 </head>
-<body class="bg-base-100">
+
+<body class="">
+    <header class="p-3">
+        <div class="navbar rounded-2xl bg-base-300">
+            <div class="flex-1">
+                <a class="btn btn-ghost normal-case text-xl">
+                    PzGiftz
+                    <img src="<?php echo get_template_directory_uri()?>/assets/img/logo.svg" class="w-9" alt="logo">
+                </a>
+            </div>
+       </div>
+    </header>
