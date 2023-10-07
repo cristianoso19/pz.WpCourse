9831a95de40b89965f342365f333ee232555338f
cristianFedora <cristianoso19@gmail.com>
2023-10-06 22:20:36 -0500

    Add new hero content with classes

--- wordpress-data/wp-content/themes/prof/front-page.php
+++ wordpress-data/wp-content/themes/prof/front-page.php
@@ -8,7 +8,13 @@
             the_post(); ?>
             <div class="hero w-full py-8 bg-base-200">
                 <div class="hero-content flex-col lg:flex-row">
-                    <?php the_content(); ?>
+                    <?php
+                        $content = get_the_content();
+                        // Agrega la clase de Tailwind a la imagen
+                        $content_with_classes = preg_replace('/<img(.*?)class=["\'](.*?)["\'](.*?)>/i', '<img$1class="$2 max-w-sm rounded-lg shadow-2xl"$3>', $content);
+                        // Muestra el contenido con la clase de Tailwind agregada a la imagen
+                        echo $content_with_classes;
+                    ?>
                     <div>
                         <h1 class="text-5xl font-bold"><?php the_title(); ?></h1>
                     </div>
