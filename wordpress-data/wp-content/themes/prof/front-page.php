a959ebcb6e81feeb76dce4e06cb69c70a9002dde
cristianFedora <cristianoso19@gmail.com>
2023-10-07 20:49:27 -0500

    implement hero image

--- wordpress-data/wp-content/themes/prof/front-page.php
+++ wordpress-data/wp-content/themes/prof/front-page.php
@@ -5,18 +5,19 @@
     <?php
     if (have_posts()) {
         while (have_posts()) {
-            the_post(); 
-            $url = get_the_post_thumbnail_url('', 'medium');
-            echo $url;
-            ?>
-            <div class="hero w-full py-8 bg-base-200">
-                <div class="hero-content flex-col lg:flex-row">
-                    <?php the_content(); ?>
-                    <div>
-                        <h1 class="text-5xl font-bold"><?php the_title(); ?></h1>
-                    </div>
-                </div>
+            the_post();
+            $url = get_the_post_thumbnail_url();
+    ?>
+
+    <div class="hero min-h-screen" style="background-image: url(<?php echo $url ?>);">
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="hero-content text-center text-neutral-content">
+            <div class="max-w-md">
+                <h class="mb-5 text-5xl font-bold"><?php the_title(); ?></h>
             </div>
+        </div>
+    </div>
+
     <?php
         }
     }
